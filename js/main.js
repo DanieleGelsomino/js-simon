f167e073233eb8342ad0e79c010d9c46939082c4
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-18 16:04:13 +0100

    add setinterval and settimeout to fix

--- js/main.js
+++ js/main.js
@@ -10,3 +10,47 @@ console.log("JS OK!");
     Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri
     da indovinare sono stati individuati.
 */
+
+function generateRandomNumber(min, max) {
+  const range = max - min + 1;
+
+  return Math.floor(Math.random() * range) + min;
+}
+
+function askToUserSequenceNumbers() {
+  const askUser = parseInt(
+    prompt("inserisci in ordine la sequenza dei numeri")
+  );
+}
+
+function timeLapsNumber() {
+  max--;
+  if (max <= 0) {
+    clearInterval(timeSequence); // cancello il setInterval
+    const container = document.getElementById("numbers");
+    container.innerText = "Tocca a te!";
+  }
+}
+
+// creo array dove aggiungo 5 numeri casuali
+
+let simonNumber = [];
+let max = 5;
+
+while (simonNumber.length < 5) {
+  number = generateRandomNumber(1, 100);
+  if (!simonNumber.includes(number)) {
+    simonNumber.push(number);
+  }
+}
+
+console.log(simonNumber);
+
+for (let i = 0; i < simonNumber.length; i++) {
+  const container = document.getElementById("numbers");
+  container.innerText = simonNumber[i];
+}
+
+const InputUser = setTimeout(askToUserSequenceNumbers, 30000);
+
+const timeSequence = setInterval(timeLapsNumber, 2000);
